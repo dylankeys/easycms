388885d8729786c4843d5930a88efa982d3a3fbd
Dylan Keys <dylankeys@btinternet.com>
2019-03-16 23:45:02 +0000

    Initial commit

---
+++ scripts/new_section.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Add a new section to SimpleCMS
+
+while [ "$1" != "" ]; do
+    case $1 in
+			-s | --section )   shift
+									SECTION=$1
+									;;
+			-d | --dataroot ) shift
+									DATAROOT=$1
+									;;
+        esac
+    shift
+done
+
+create_section() {
+    mkdir $DATAROOT/sections/$SECTION
+}
+
+symlink_section() {
+	ln -s $DATAROOT/sections/$SECTION ../
+}
+
+create_section
+
+
