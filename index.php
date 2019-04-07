362d055ab886866286441d6f8db2c795ed0ef66c
Dylan Keys <dylankeys@btinternet.com>
2019-04-07 20:37:30 +0100

    Removing navigation code from index page, as now resides in lib

--- index.php
+++ index.php
@@ -6,25 +6,6 @@
   require_once('vendor/autoload.php');
 
   $m = new Mustache_Engine;
-  $data = array();
-  $template = file_get_contents('templates/home.mustache');
-
-  $dbQuery=$db->prepare("SELECT `name` FROM `sections` ORDER BY `position` ASC");
-  $dbQuery->execute();
-
-  while ($dbRow = $dbQuery->fetch(PDO::FETCH_ASSOC))
-  {
-    $section_name=$dbRow["name"];
-    $section_link=lcfirst($section_name);
-
-    $section_data = array(
-      'section_name' => $section_name,
-      'section_link' => $section_link
-    );
-
-    $data['sections'][] = $section_data;
-
-  }
 
   echo $m->render($template, $data);
 
