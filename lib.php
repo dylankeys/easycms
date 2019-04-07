847dea66c24f38cc0b3cf9b3c40f035e1eb616c6
Dylan Keys <dylankeys@btinternet.com>
2019-04-07 20:35:37 +0100

    Adding template data declaration and global variables to lib

---
+++ lib.php
@@ -0,0 +1,26 @@
+<?php
+  include('config.php');
+
+  global $CFG;
+
+  $CFG->dirroot = __DIR__;
+  $data = array();
+
+  $data['year'] = date('Y');
+
+  $dbQuery=$db->prepare("SELECT `name` FROM `sections` ORDER BY `position` ASC");
+  $dbQuery->execute();
+
+  while ($dbRow = $dbQuery->fetch(PDO::FETCH_ASSOC))
+  {
+    $section_name=$dbRow["name"];
+    $section_link=lcfirst($section_name);
+
+    $section_data = array(
+      'section_name' => $section_name,
+      'section_link' => $section_link
+    );
+
+    $data['sections_nav'][] = $section_data;
+
+  }
