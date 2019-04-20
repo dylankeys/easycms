ec72e7370e491329d7fcf2533a4673453b4dfaba
Dylan Keys <dylankeys@btinternet.com>
2019-04-20 19:36:57 +0100

    Adding intial content code and template

--- admin/add/content/index.php
+++ admin/add/content/index.php
@@ -1,100 +1,46 @@
 <?php
   session_start();
-  include("../../../config.php");
-  include("../../../lib.php");
-?>
-<!doctype html>
-<!-- EasyCMS -->
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Pure and True Holistic Therapies offer homemade natural products as well as a variety of relaxing treatments. Come and join the community to learn more.">
-    <meta name="keywords" content="Holistic,Therapies,Therapy,Aromatherapy,Relax,Homemade,Natural,Organic,Pure,True">
-    <meta name="author" content="Angela Keys">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  include($_SERVER['DOCUMENT_ROOT'].'/lib.php');
 
-    <!-- Font Awesome CSS -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+  require_once($CFG->dirroot.'/vendor/autoload.php');
+
+  $m = new Mustache_Engine;
+
+  $template = file_get_contents($CFG->dirroot.'/templates/admin/content.mustache');
+
+  $dbQuery=$db->prepare("SELECT id, name FROM `sections` ORDER BY `position` ASC");
+  $dbQuery->execute();
+
+  while ($dbRow = $dbQuery->fetch(PDO::FETCH_ASSOC))
+  {
+    $section_id = $dbRow["id"];
+    $section_name = $dbRow["name"];
+
+    $section_data = array(
+      'section_id' => $section_id,
+      'section_name' => $section_name
+    );
+
+    $dbQueryContent=$db->prepare("SELECT id, name, main FROM `content` WHERE sectionid = :sectionid");
+    $dbParamsContent = array('sectionid'=>$section_id);
+    $dbQueryContent->execute($dbParamsContent);
+
+    while ($dbRowContent = $dbQueryContent->fetch(PDO::FETCH_ASSOC))
+    {
+      $section_id = $dbRowContent["id"];
+      $section_name = $dbRowContent["name"];
+
+      $content_data = array(
+        'content_id' => $content_id,
+        'content_name' => $content_name
+      );
+
+      $section_data['content_manage'][] = $content_data;
+    }
 
 
+    $data['sections_manage'][] = $section_data;
+  }
 
-    <title>Pure & True | Holistic Therapies</title>
-
-    <link rel="icon" href="../../../pix/favicon.gif" type="image/gif">
-
-    <!-- EasyCMS CSS -->
-    <link rel="stylesheet" type="text/css" href="../../../css/easycms.css">
-
-    <!-- Custom CSS -->
-    <link rel="stylesheet" type="text/css" href="../../../css/bs-blog.css">
-
-  </head>
-  <body>
-    <div class="container">
-      <header class="blog-header py-3">
-        <div class="row flex-nowrap justify-content-between align-items-center">
-          <div class="col-4 pt-1">
-            <a class="text-muted" href="#">Sign-up for updates</a>
-          </div>
-          <div class="col-4 text-center">
-            <img class="logo" src="../../../pix/logo.png" alt="Pure and True Holistic Therapies logo" />
-          </div>
-          <div class="col-4 d-flex justify-content-end align-items-center">
-            <a class="btn btn-sm btn-outline-secondary" href="#">Book now</a>
-          </div>
-        </div>
-      </header>
-
-      <div class="nav-scroller py-1 mb-2">
-        <nav class="nav d-flex justify-content-between">
-          <?php
-            $dbQuery=$db->prepare("SELECT `name` FROM `sections` ORDER BY `position` ASC");
-            $dbQuery->execute();
-            //$dbRow=$dbQuery->fetch(PDO::FETCH_ASSOC);
-
-            while ($dbRow = $dbQuery->fetch(PDO::FETCH_ASSOC))
-            {
-              $name=$dbRow["name"];
-
-              echo '<a class="p-2 text-muted" href="?s='.lcfirst($name).'">'.$name.'</a>';
-            }
-          ?>
-        </nav>
-      </div>
-
-      <div class="container">
-     
-        <br>
-          <h1>Settings</h1>
-     
-        <div class="jumbotron jumbotron-fluid slideshow-right">
-          <div class="container-jumbo">
-            <h1 class="display-4">EasyCMS (alpha)</h1>
-            <p class="lead">Version: 0.1</p>
-          </div>
-        </div>
-     
-        <div class="list-group slideshow-left">
-          <a href="addsection/" class="list-group-item list-group-item-action">Add sections</a>
-          <a href="addpage/" class="list-group-item list-group-item-action">Add pages</a>
-        </div>
-      </div>
-    </div>
-
-    <footer class="blog-footer footer">
-      <p>&copy; <?php echo date('Y'); ?> Pure & True Holistics | Site powered by <a href="https://dylankeys.com">EasyCMS</a></p>
-      <p>
-        <a href="#">Back to top</a>
-      </p>
-    </footer>
-    <!-- Bootstrap JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-  </body>
-</html>
+  echo $m->render($template, $data);
