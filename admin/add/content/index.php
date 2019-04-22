964a9090cd3e411952d08baa70f32b2209193058
Dylan Keys <dylankeys@btinternet.com>
2019-04-22 12:26:14 +0100

    NEW FEATURE: Add a page

--- admin/add/content/index.php
+++ admin/add/content/index.php
@@ -9,38 +9,82 @@
 
   $template = file_get_contents($CFG->dirroot.'/templates/admin/content.mustache');
 
-  $dbQuery=$db->prepare("SELECT id, name FROM `sections` ORDER BY `position` ASC");
-  $dbQuery->execute();
+  if (isset($_POST['submit'])) {
+    if (isset($_POST['add_page'])) {
+      $page_name = $_POST['page_name'];
+      $template = $_POST['templates'];
+      $section_id = $_POST['section_id'];
+      $main = 0; //Always set to zero as this is not the area for creating a new main section page
 
-  while ($dbRow = $dbQuery->fetch(PDO::FETCH_ASSOC))
-  {
-    $section_id = $dbRow["id"];
-    $section_name = $dbRow["name"];
+      $dbQuery=$db->prepare("INSERT INTO `content` VALUES(null,:name,:sectionid,:main,:templateid)");
+      $dbParams = array('name'=>$page_name,'sectionid'=>$section_id,'main'=>$main,'templateid'=>$template);
+      $dbQuery->execute($dbParams);
 
-    $section_data = array(
-      'section_id' => $section_id,
-      'section_name' => $section_name
-    );
+      header("Location: ?success=pagecreated");
+    }
+  }
 
-    $dbQueryContent=$db->prepare("SELECT id, name, main FROM `content` WHERE sectionid = :sectionid");
-    $dbParamsContent = array('sectionid'=>$section_id);
-    $dbQueryContent->execute($dbParamsContent);
+  if (isset($_GET['action'])) {
+    if ($_GET['action'] == "add") {
+      $section_id = $_GET['sid'];
 
-    while ($dbRowContent = $dbQueryContent->fetch(PDO::FETCH_ASSOC))
+      $dbQuery=$db->prepare("SELECT id, name FROM `templates`");
+      $dbQuery->execute();
+
+      while ($dbRow = $dbQuery->fetch(PDO::FETCH_ASSOC))
+      {
+        $template_id = $dbRow["id"];
+        $template_name = $dbRow["name"];
+
+        $template_data['templates'][] = array (
+          'template_id' => $template_id,
+          'template_name' => $template_name
+        );
+
+        $data['add_page'][] = $template_data;
+      }
+
+      $data['add_page']['section_id'] = $section_id;
+  }
+  else {
+
+    $dbQuery=$db->prepare("SELECT id, name FROM `sections` ORDER BY `position` ASC");
+    $dbQuery->execute();
+
+    while ($dbRow = $dbQuery->fetch(PDO::FETCH_ASSOC))
     {
-      $section_id = $dbRowContent["id"];
-      $section_name = $dbRowContent["name"];
+      $section_id = $dbRow["id"];
+      $section_name = $dbRow["name"];
 
-      $content_data = array(
-        'content_id' => $content_id,
-        'content_name' => $content_name
+      $section_data = array(
+        'section_id' => $section_id,
+        'section_name' => $section_name
       );
 
-      $section_data['content_manage'][] = $content_data;
+      $dbQueryContent=$db->prepare("SELECT id, name, main FROM `content` WHERE sectionid = :sectionid");
+      $dbParamsContent = array('sectionid'=>$section_id);
+      $dbQueryContent->execute($dbParamsContent);
+
+      while ($dbRowContent = $dbQueryContent->fetch(PDO::FETCH_ASSOC))
+      {
+        $main_page = $dbRowContent["main"];
+
+        if (!$main_page) {
+          $content_id = $dbRowContent["id"];
+          $content_name = $dbRowContent["name"];
+
+          $content_data = array(
+            'content_id' => $content_id,
+            'content_name' => $content_name
+          );
+
+          $section_data['content_manage'][] = $content_data;
+        }
+      }
+
+
+      $data['sections_manage'][] = $section_data;
     }
-
-
-    $data['sections_manage'][] = $section_data;
   }
 
   echo $m->render($template, $data);
